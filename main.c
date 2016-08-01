c707409656de6fb68e13d9a19056757974af14d3
Frederic Suter <frederic.suter@cc.in2p3.fr>
2016-08-01 17:27:43 +0200

    should solve the issue

--- main.c
+++ main.c
@@ -13,34 +13,32 @@ XBT_LOG_NEW_DEFAULT_CATEGORY(remote_io, "Messages specific for this io example")
 int create_copy();
 static int host(int argc, char *argv[]){
   msg_file_t file;
-
+  int i;
   if (argc > 2){
-
+    for (i = 0; i<3 ;i++){  
       char** data = xbt_new(char*, 3);
-      data[0] = argv[1];
-      data[1] = argv[2];
-      data[2] = argv[3];
+      data[0] = xbt_strdup(argv[1]);
+      data[1] = xbt_strdup(argv[2]);
+      data[2] = xbt_strdup(argv[3]);
 
-      MSG_process_create("copy1", create_copy, data, MSG_host_self());
-      MSG_process_create("copy2", create_copy, data, MSG_host_self());
-      MSG_process_create("copy3", create_copy, data, MSG_host_self());
+      MSG_process_create_with_arguments("copy", create_copy , NULL, MSG_host_self(), 3, data);
+    }
   }
 
   return 0;
 }
 
-int create_copy(){
-    char** argv = MSG_process_get_data(MSG_process_self());
-    msg_file_t file = MSG_file_open(argv[0], NULL);
+int create_copy(int argc, char**argv){
+  msg_file_t file = MSG_file_open(argv[0], NULL);
     const char* filename = MSG_file_get_name(file);
     msg_host_t src, dest;
     src= MSG_host_self();
     dest = MSG_host_by_name(argv[1]);
 
-    XBT_INFO("Copy '%s' (of size %llu) from '%s' to '%s'", filename, MSG_file_get_size(file), MSG_host_get_name(src),
-             argv[1]);
+    XBT_INFO("Copy '%s' (of size %llu) from '%s' to '%s' as '%s'", filename, MSG_file_get_size(file), MSG_host_get_name(src), MSG_host_get_name(dest), argv[2]);
     MSG_file_rcopy(file, dest, argv[2]);
     MSG_file_close(file);
+
     return 0;
 }
 
@@ -56,13 +54,13 @@ int main(int argc, char **argv)
 
   xbt_dynar_t storages = MSG_storages_as_dynar();
   xbt_dynar_foreach(storages, cur, st){
-    XBT_INFO("Init: %llu MiB used on '%s'", MSG_storage_get_used_size(st)/INMEGA, MSG_storage_get_name(st));
+    XBT_INFO("Init: %llu MiB used on '%s'", MSG_storage_get_used_size(st), MSG_storage_get_name(st));
   }
 
   int res = MSG_main();
 
   xbt_dynar_foreach(storages, cur, st){
-    XBT_INFO("Init: %llu MiB used on '%s'", MSG_storage_get_used_size(st)/INMEGA, MSG_storage_get_name(st));
+    XBT_INFO("Init: %llu MiB used on '%s'", MSG_storage_get_used_size(st), MSG_storage_get_name(st));
   }
   xbt_dynar_free_container(&storages);
 
